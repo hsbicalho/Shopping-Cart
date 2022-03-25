0c8469d61a4cb0146e05884fc8a571659361fa83
Heitor <hsbicalho@gmail.com>
2022-03-25 17:28:40 -0300

    req 8 e 9 refatorando

--- tests/fetchProducts.test.js
+++ tests/fetchProducts.test.js
@@ -3,6 +3,11 @@ const { fetchProducts } = require('../helpers/fetchProducts');
 const computadorSearch = require('../mocks/search');
 
 describe('1 - Teste a função fecthProducts', () => {
-  // implemente seus testes aqui
-  fail('Teste vazio');
+  it('test if fetchProduts is a function', async () => {
+    expect(typeof fetchProducts).toBe('function');
+  })
+  it('test if calls fetchProducts', async () => {
+    await fetchProducts('computador');
+    expect(fetch).toHaveBeenCalled();
+  })
 });

c720033c060025b1f4632c0cb2a7f60b5fdeedfd
Heitor <hsbicalho@gmail.com>
2022-03-25 11:39:40 -0300

    requisito 3 implementado

--- script.js
+++ script.js
@@ -30,15 +30,19 @@ function getSkuFromProductItem(item) {
   return item.querySelector('span.item__sku').innerText;
 }
 
-async function cartItemClickListener(event) {
-  // nada
+async function cartItemClickListener({ target }) {
+  document.querySelector('.cart__items').removeChild(target);
 }
 
-function createCartItemElement({ id: sku, title: name, price: salePrice }) {
+function createCartItemElement({ id: sku, title: name, price: salePrice, thumbnail: image }) {
   const li = document.createElement('li');
+  const img = document.createElement('img');
+  img.src = image;
+  img.className = 'cart_item_img';
   li.className = 'cart__item';
   li.innerText = `SKU: ${sku} | NAME: ${name} | PRICE: $${salePrice}`;
   li.addEventListener('click', cartItemClickListener);
+  li.appendChild(img);
   return li;
 }
 
@@ -47,14 +51,15 @@ async function addElementToCart(id) {
   const fechItem = await item.json();
   const liItem = document.querySelector('.cart__items');
   liItem.appendChild(createCartItemElement(fechItem));
-  await sumAsync();
 }
 
 document.addEventListener('click', ({ target }) => {
   if (target.classList.contains('item__add')) {
     return addElementToCart(getSkuFromProductItem(target.parentElement));
   }
-  sumAsync();
+  if (target.classList.contains('empty-cart')) {
+    document.querySelector('section .cart__items').innerHTML = '';
+  }
 });
 
 async function getProducts(name) {
